5e01fd9c5b512a345867670296150edf0720ebc1
Richy Teas <richyteas@gmail.com>
2016-05-05 03:07:07 -0700

    github basic api implementation

---
+++ app/controllers/View1Ctrl.js
@@ -0,0 +1,4 @@
+angular.module('myApp.view1')
+	.controller('View1Ctrl', ['$scope', function($scope) {
+	$scope.name = "see this";
+}]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,9 +6,29 @@ angular.module('myApp.view1', ['ngRoute'])
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
+  }).
+  when('/github', {
+  	templateUrl: 'view1/github.html',
+  	controller: 'gitCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+	$scope.name = "see this";
+
+	$scope.user = "rteas";
+
+	$scope.gitData = function(){
+		$http.get('https://api.github.com/users/'+$scope.user).
+		then(function(response){
+			$scope.profile = response.data;
+		});
+		$http.get('https://api.github.com/users/'+$scope.user+'/repos').
+		then(function(response){
+			$scope.repos = response.data;
+		});
+	}
+
+	$scope.gitData();
 
 }]);
